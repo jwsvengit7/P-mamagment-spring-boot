70dce0eaef9baa6d6e54f470ecd13cf4790e182a
jwsven <chiorlujack@gmail.com>
2024-03-31 10:46:27 +0100

    updated security

--- src/main/java/com/p/backend/productsmanagement/config/SecurityConfig.java
+++ src/main/java/com/p/backend/productsmanagement/config/SecurityConfig.java
@@ -3,12 +3,16 @@ package com.p.backend.productsmanagement.config;
 
 
 import com.p.backend.productsmanagement.security.JwtAuthFilter;
+import com.p.backend.productsmanagement.security.JwtEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,30 +22,41 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
+    private final JwtEntryPoint jwtEntryPoint;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.cors(corsConfigurer -> corsConfigurer.configure(httpSecurity));
-        httpSecurity.csrf(AbstractHttpConfigurer::disable);
-        httpSecurity.authorizeHttpRequests(authorize ->
-                authorize.requestMatchers(
-                                "/api/v1/auth/**",
-                                "/v2/api-docs",
-                                "/v3/api-docs",
-                                "/v3/api-docs/**",
-                                "/swagger-resources",
-                                "/swagger-resources/**",
-                                "/configuration/ui",
-                                "/configuration/security",
-                                "/swagger-ui/**",
-                                "/webjars/**",
-                                "/swagger-ui.html"
-                        )
-                        .permitAll()
-                        .anyRequest()
-                        .authenticated()
-                        .and()
-                        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class));
-        return httpSecurity.build();
-    }
+
+
+        httpSecurity
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .exceptionHandling(configurer -> configurer.authenticationEntryPoint(jwtEntryPoint))
+                    .authorizeHttpRequests(authorize -> {
+                        authorize.requestMatchers(  "/api/v1/auth/**",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated();
+                    })
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authenticationProvider(authenticationProvider)
+                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                    .logout(logout -> {
+                        logout.logoutUrl("/logout");
+                        logout.logoutSuccessHandler((((request, response, authentication) -> SecurityContextHolder.clearContext())));
+                    });
+            return httpSecurity.build();
+        }
+
 }
